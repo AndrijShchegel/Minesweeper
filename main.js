e146fba1c0c8fb8dbcda12d3f0cb6de5a2b991dd
AndrijShchegel <89912205+AndrijShchegel@users.noreply.github.com>
2022-06-20 19:04:54 +0300

    add board

--- main.js
+++ main.js
@@ -5,3 +5,16 @@ const rows = 8;
 const columns = 8;
 const minesCount = 10;
 const minesLocation = [];
+
+const startGame = () => {
+  for (let col = 0; col < columns; col++) {
+    board[col] = [];
+    for (let row = 0; row < rows; row++) {
+      const tile = document.createElement('div');
+      tile.id = col + '-' + row;
+      tile.addEventListener('click', clickTile);
+      document.getElementById('board').append(tile);
+      board[col][row] = tile;
+    }
+  }
+};
