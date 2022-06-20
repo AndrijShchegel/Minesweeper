8ca027fe986c7204330af6f936afe606e4c08d33
AndrijShchegel <89912205+AndrijShchegel@users.noreply.github.com>
2022-06-20 19:05:49 +0300

    set mines to the board

--- main.js
+++ main.js
@@ -6,7 +6,23 @@ const columns = 8;
 const minesCount = 10;
 const minesLocation = [];
 
+const random = size => (Math.floor(Math.random() * size));
+
+const setMines = () => {
+  let minesLeft = minesCount;
+  while (minesLeft > 0) {
+    const row = random(rows);
+    const col = random(columns);
+    const id = col + '-' + row;
+    if (!minesLocation.includes(id)) {
+      minesLocation.push(id);
+      minesLeft -= 1;
+    }
+  }
+};
+
 const startGame = () => {
+  setMines();
   for (let col = 0; col < columns; col++) {
     board[col] = [];
     for (let row = 0; row < rows; row++) {
